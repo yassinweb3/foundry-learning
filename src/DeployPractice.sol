e09f137cccdfe6f787987a695e22165c0c9d1c6c
Yassin Jamal Hussain <yasynj248@gmail.com>
2026-09-21 07:47:58 +0300

    Add Sepolia deployment practice contract

---
+++ src/DeployPractice.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+contract DeployPractice {
+    uint256 public number;
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
+}
